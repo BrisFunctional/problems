0e20ae0d6bfff96e0d9e826006018c4ba08ee982
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-03-18 18:06:55 +0000

    better explanation of prime facotring code

--- teams/mjg-clojure/brisfunctional/src/brisfunctional/problem3.clj
+++ teams/mjg-clojure/brisfunctional/src/brisfunctional/problem3.clj
@@ -1,6 +1,10 @@
 (ns brisfunctional.problem3)
 
-(defn find-largest-prime-factor [n]
+(defn find-largest-prime-factor
+  "Recursively remove factors of n starting from 2 and counting up.
+   We don't need to check explicitly that they are primes,
+   as all smaller divisors are factored out first"
+  [n]
   
   (loop [number n
          divisor 2]
@@ -9,6 +13,6 @@
       number
       
       (if (= 0 (mod number divisor))
-        
+  
         (recur (/ number divisor) divisor)
         (recur number (+ 1 divisor))))))
