9c439b58b64bdaf1f01147b489311fdf183b6fc7
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-03-18 22:01:36 +0000

    problem 5 done

--- teams/mjg-clojure/brisfunctional/src/brisfunctional/problem5.clj
+++ teams/mjg-clojure/brisfunctional/src/brisfunctional/problem5.clj
@@ -1,1 +1,23 @@
 (ns brisfunctional.problem5)
+
+(defn gcd
+  "Greatest common divisor, via Euclid's algorithm"
+  [a b]
+  
+  ; we must have a > b
+  (if (> b a)
+    (gcd b a)
+    
+    (loop [a a
+           b b]
+      
+      (if (= 0 b)
+        a
+        (recur b (mod a b)))))) 
+
+(defn lcm
+  "lowest common multiple, according to identity that lcm(a,b) = ab/gcd(a,b)"
+  [a b]
+  (/ 
+    (* a b)
+    (gcd a b)))
--- teams/mjg-clojure/brisfunctional/test/brisfunctional/test/problem5_test.clj
+++ teams/mjg-clojure/brisfunctional/test/brisfunctional/test/problem5_test.clj
@@ -3,3 +3,23 @@
   (:use [clojure.test])
   (:use midje.sweet))
 
+
+(fact 
+  (gcd 21 49) => 7)
+
+(fact 
+  (gcd 100 140) => 20)
+
+
+(fact
+  (lcm 5 9) => 45)
+
+(fact
+  (lcm 3 21) => 21)
+
+
+(fact
+  (reduce lcm (range 1 10)) => 2520)
+
+(fact
+  (reduce lcm (range 1 20)) => 232792560)
