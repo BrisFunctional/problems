8dc009139efc1cc9d4a92eacf8cfb540ec604dc9
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-03-18 21:39:01 +0000

    more clarity in problem 4 solution

--- teams/mjg-clojure/brisfunctional/src/brisfunctional/problem4.clj
+++ teams/mjg-clojure/brisfunctional/src/brisfunctional/problem4.clj
@@ -15,17 +15,19 @@
   (= n (number-reverser n)))
 
 (defn is-product-of-two-3-digit-numbers
-  "Trial division from 100-999, check if 100 <= remainder <= 999" 
+  "Trial division from (max n/999 100) to (min n/100 999)" 
   [n]
   
-  (loop [div1 100]
-    (if (and 
-          (= 0 (mod n div1))
-          (<= 100 (/ n div1))
-          (>= 999 (/ n div1)))
-      true
-      
-      (if (= 999 div1)
-        false
+  (let [lower-bound (max (int (/ n 999)) 100)
+        upper-bound (min (/ n 100) 999)] 
+  
+    (loop [div lower-bound]
+    
+      (if (= 0 (mod n div))
+        true
         
-        (recur (+ 1 div1))))))
+        (if (< upper-bound div)
+          false
+          
+          (recur (+ 1 div)))))))
+  
