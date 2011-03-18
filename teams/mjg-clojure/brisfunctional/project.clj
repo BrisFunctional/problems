e02b15dcfd8e57a4ac8647db192b1dcb5e640f1e
Matthew Gilliard <matthew.gilliard@gmail.com>
2011-03-18 10:18:43 +0000

    added code for problems 1&2 in clojure

---
+++ teams/mjg-clojure/brisfunctional/project.clj
@@ -0,0 +1,6 @@
+(defproject brisfunctional "1.0.0-SNAPSHOT"
+  :description "Brisfunctional problems hacked out in clojure"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-eclipse "1.0.0"]
+                     [midje "1.1-alpha-2"]])
---
+++ teams/mjg-clojure/brisfunctional/test/brisfunctional/test/problem2_test.clj
@@ -0,0 +1,23 @@
+(ns brisfunctional.test.problem2-test
+  (:use [brisfunctional.problem2] :reload)
+  (:use [clojure.test])
+   (:use midje.sweet))
+
+
+(fact
+  (take 2 fibonacci) => '(1 2))
+
+(fact
+  (take 10 fibonacci) => '(1 2 3 5 8 13 21 34 55 89))
+
+(fact
+  (take-while #(< % 10) fibonacci) => '(1 2 3 5 8))
+
+(fact
+  (take-while #(< % 10) even-fibonacci) => '(2 8))
+
+(fact
+  (reduce + 
+      (take-while #(< % 4000000) even-fibonacci))
+  => 4613732)
+
