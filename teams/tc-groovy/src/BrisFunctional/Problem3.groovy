f2fd286977cbcb453fe9cc8211a203a7ea23ac24
Tom Coupland <tcoupland@gmail.com>
2011-03-22 20:18:06 +0000

    Added solution for problem 3

---
+++ teams/tc-groovy/src/BrisFunctional/Problem3.groovy
@@ -0,0 +1,12 @@
+primeFactor = {value, factor=2 -> 
+    while (factor != value){
+    while(value % factor == 0){
+        value = (BigInteger)(value/factor)
+    }
+    factor++
+    }
+    factor
+}
+
+assert primeFactor(13195) == 29
+assert primeFactor((BigInteger)600851475143) == 6857
